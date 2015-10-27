29d11c1fdf0c02f4a4da58a169103d1c972d48a6
julyat <julyat@sina.com>
2015-10-27 08:34:32 +0800

    漏上传
    15-10-27

---
+++ his4tools/his4tools.bat
@@ -0,0 +1,38 @@
+@echo off
+D:
+md  d:\his4tools
+cd  D:\his4tools
+taskkill /IM his4tools.exe /F 
+if exist canshu.ini goto canshu else
+copy \\192.189.101.98\backup\his4tools\*.*  /y
+copy ms*.* %windir%\system32\ /y
+Regsvr32 %windir%\system32\msadodc.ocx /s
+Regsvr32 %windir%\system32\mscomm32.ocx /s
+Regsvr32 %windir%\system32\msdatgrd.ocx /s
+Regsvr32 %windir%\system32\msstdfmt.dll /s
+copy ms*.* %windir%\syswow64\ /y
+Regsvr32 %windir%\syswow64\msadodc.ocx /s
+Regsvr32 %windir%\syswow64\mscomm32.ocx /s
+Regsvr32 %windir%\syswow64\msdatgrd.ocx /s
+Regsvr32 %windir%\syswow64\msstdfmt.dll /s
+Regsvr32 %windir%\syswow64\Richtx32.ocx /s
+start his4tools.exe
+exit
+
+:canshu
+rename canshu.ini canshu.ini.bak
+copy \\192.189.101.98\backup\his4tools\*.*  /y
+copy canshu.ini.bak canshu.ini /y
+copy ms*.* %windir%\system32\ /y
+Regsvr32 %windir%\system32\msadodc.ocx /s
+Regsvr32 %windir%\system32\mscomm32.ocx /s
+Regsvr32 %windir%\system32\msdatgrd.ocx /s
+Regsvr32 %windir%\system32\msstdfmt.dll /s
+copy ms*.* %windir%\syswow64\ /y
+Regsvr32 %windir%\syswow64\msadodc.ocx /s
+Regsvr32 %windir%\syswow64\mscomm32.ocx /s
+Regsvr32 %windir%\syswow64\msdatgrd.ocx /s
+Regsvr32 %windir%\syswow64\msstdfmt.dll /s
+Regsvr32 %windir%\syswow64\Richtx32.ocx /s
+start his4tools.exe
+exit
